376ae4a93c0890c7abf1445d553ab3959f7e6de3
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 00:31:55 +0700

    refactor: add new lines of codes in AmITrue.java file

--- print/AmITrue.java
+++ print/AmITrue.java
@@ -4,9 +4,14 @@ public class AmITrue {
     public static void main(String[] args) {
 
         boolean amIHuman = true;
+        boolean areYouMyFriend = false;
 
         if (amIHuman) {
             System.out.println("Hello, I am human!");
+        }
+
+        if (!areYouMyFriend) {
+            System.out.println("You are not my friend!");
         }
 
     }
