08474063e565da875607aa7b8e530d88b6377ff3
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 00:26:23 +0700

    feat: add AmITrue.java file

---
+++ loop/AmITrue.java
@@ -0,0 +1,13 @@
+
+public class AmITrue {
+
+    public static void main(String[] args) {
+
+        boolean amIHuman = true;
+
+        if (amIHuman) {
+            System.out.println("Hello, I am human!");
+        }
+
+    }
+}
