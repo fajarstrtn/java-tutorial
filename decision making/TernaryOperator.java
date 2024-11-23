c4e5ec4112853fb4f7a49b2cba3039c130054c4a
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 11:38:28 +0700

    feat: add TernaryOperator.java file

---
+++ decision making/TernaryOperator.java
@@ -0,0 +1,15 @@
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        boolean isLamborghiniGood = true;
+
+        if (isLamborghiniGood) {
+            System.out.println("This car is good!");
+    
+    
+        }        
+ 
+   }
+}
