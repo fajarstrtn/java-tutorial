1599143a553c12b0a9aba5454d4c7877e116caf6
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 11:46:06 +0700

    refactor: send the result of ternary operator to the new variable

--- decision making/TernaryOperator.java
+++ decision making/TernaryOperator.java
@@ -4,8 +4,9 @@ public class TernaryOperator {
     public static void main(String[] args) {
 
         boolean isLamborghiniGood = true;
+        String answer = isLamborghiniGood? "This is a good car" : "No, this is bad";
 
-        System.out.println(isLamborghiniGood? "This is a good car" : "No, this is bad");
+        System.out.println(answer);
  
    }
 }
