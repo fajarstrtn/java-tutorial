17f8da65d461abf0b1d3e42f064e2e60b57f7b96
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 23:56:06 +0700

    feat: add DefaultHandler.java file

---
+++ exception/DefaultHandler.java
@@ -0,0 +1,11 @@
+public class DefaultHandler {
+
+    public static void main(String[] args) {
+        
+/*      If user does not specify the suitable exception,
+        Java will throw the exception to the default exception handler. */
+        String value = null;
+        System.out.println(value.length());
+
+    }
+}
