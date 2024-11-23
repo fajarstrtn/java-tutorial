b318ac3f6c7ccebf33a47d3657e8de9c5c63d271
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-24 00:30:23 +0700

    feat: add ExceptionFlowPt3.java file

---
+++ exception/ExceptionFlowPt3.java
@@ -0,0 +1,35 @@
+
+public class ExceptionFlowPt3 {
+
+    public static void main(String[] args) {
+
+        try {
+
+/*          Java will terminate the program abnormally when user does not define the suitable exception.
+            All the rest of the program will not be executed. */
+            int value = calculateNumber(10, 0);
+
+            System.out.println(value);
+
+        } catch (NullPointerException e) {
+
+            System.err.println(e.toString());
+
+        }
+
+        System.out.println("Program ends");
+
+    }
+
+/*  This calculateNumber method adds an ArithmeticException method signature,
+    where the caller of this method should handle the same exception as its declared.
+    
+    But as user can see, there is no catch block which handles ArithmeticException.
+    Instead, there is only NullPointerException which exactly does not match with the appropriate exception.
+    Then, Java will throw the exception object to the default exception handler. */
+    private static int calculateNumber(int firstNumber, int secondNumber) throws ArithmeticException {
+
+        return firstNumber / secondNumber;
+
+    }
+}
