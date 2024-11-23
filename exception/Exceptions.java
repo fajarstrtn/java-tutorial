5fcb55ee325e869ddc027a901918432185bfa954
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 22:36:40 +0700

    feat: add Exceptions.java file

---
+++ exception/Exceptions.java
@@ -0,0 +1,19 @@
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+
+        try {
+
+            String value = null;
+            System.out.println(value.length());
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.err.println(e.toString());
+            System.err.println(e.getMessage());
+
+        }
+    }
+}
