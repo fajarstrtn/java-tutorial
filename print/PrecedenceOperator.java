74f3474bb1f00ef92f68e0fd86cff7aa686f95b0
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 14:34:20 +0700

    feat: add PrecedenceOperator.java file

---
+++ print/PrecedenceOperator.java
@@ -0,0 +1,19 @@
+
+public class PrecedenceOperator {
+
+    public static void main(String[] args) {
+
+        double firstNumber = 20;
+        double secondNumber = 80;
+        double result = ((firstNumber + secondNumber) * 100) % 40;
+        System.out.println(result);
+
+        boolean isResultContainingZero = result == 0;
+        System.out.println(isResultContainingZero);
+
+        if (!isResultContainingZero) {
+            System.out.println("Got some remainder");
+        }
+
+    }
+}
