81af176dc8b2e2e27c82562b0d60eff7b75ce554
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-23 11:50:50 +0700

    refactor: validate two different strings

--- decision making/TernaryOperator.java
+++ decision making/TernaryOperator.java
@@ -7,6 +7,12 @@ public class TernaryOperator {
         String answer = isLamborghiniGood? "This is a good car" : "No, this is bad";
 
         System.out.println(answer);
- 
+        
+        String myCar = "Honda Jazz";
+        String yourCar = "Tesla Model S";
+
+        System.out.println(myCar.equals(yourCar));
+        System.out.println(!myCar.equals(yourCar));
+        
    }
 }
