b772fa91fb3c0fcd220e190b110e822d9beb8d0f
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-11-30 04:01:09 +0700

    feat: add StudentIndex.java file

---
+++ method/StudentIndex.java
@@ -0,0 +1,36 @@
+public class StudentIndex {
+
+    public static void main(String[] args) {
+        
+        long result = calculateScore("Harry", 50);
+        
+        int position = getPosition(result);
+
+        System.out.println("Final score is " + result);
+
+        System.out.println("My position is " + position);
+
+    }
+
+    private static long calculateScore (String firstName, int finalScore) {
+
+        System.out.println (String.format("Hi! My name is %s", firstName));
+
+        return finalScore *= 10;
+
+    }
+
+    private static int getPosition (long finalScore) {
+
+        int position = 5;
+
+        if (finalScore >= 900 && finalScore <= 1000)
+            return 1;
+        else if (finalScore >= 800 && finalScore < 900)
+            return 2;
+        else if (finalScore >= 700 && finalScore < 800)
+            return 3;
+        
+        return position;
+    }
+}
