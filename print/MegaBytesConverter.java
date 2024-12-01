c2c901591721e39e577a634625cb0e7622926c1d
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-02 01:20:23 +0700

    feat: add MegaBytesConverter.java file

---
+++ print/MegaBytesConverter.java
@@ -0,0 +1,24 @@
+
+public class MegaBytesConverter {
+
+    public static void main (String[] args) {
+        
+        printMegaBytesAndKiloBytes (10_000);
+        printMegaBytesAndKiloBytes (-10_000);
+
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if (kiloBytes < 0) {
+
+            System.out.println ("Invalid Value");
+
+            return;
+
+        }
+
+        System.out.println (kiloBytes + " KB = " + (kiloBytes / 1024) + " MB and " + (kiloBytes % 1024) + " KB");
+
+    }
+}
