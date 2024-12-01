050af7400d4684b1f03031973291993e50d08abf
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-02 01:30:35 +0700

    feat: add BarkingDog.java file

---
+++ print/BarkingDog.java
@@ -0,0 +1,24 @@
+
+public class BarkingDog {
+
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (!barking) {
+            return false;
+        }
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+
+        return hourOfDay < 8 || hourOfDay > 22;
+
+    }
+}
