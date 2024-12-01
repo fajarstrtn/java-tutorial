25b4b0a315eba9fdad94ec07c9907ee78062794b
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-02 01:55:33 +0700

    feat: add LeapYear.java file

---
+++ print/LeapYear.java
@@ -0,0 +1,40 @@
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        int year = 366;
+
+        System.out.println("is " + year + " a lear year? " + isLeapYear(year));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+
+        if (year >= 1 && year <= 9999) {
+
+            if (year % 4 == 0) {
+
+                if (year % 100 == 0) {
+
+                    return year % 400 == 0;
+
+                } else {
+
+                    return true;
+
+                }
+
+            } else {
+
+                return false;
+
+            }
+
+        } else {
+
+            return false;
+
+        }
+    }
+}
