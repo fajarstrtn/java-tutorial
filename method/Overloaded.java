d0d615382be692f9c568665bc52822f932a6e85e
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-02 17:45:01 +0700

    refactor: add overloading method in Overloaded.java file

--- method/Overloaded.java
+++ method/Overloaded.java
@@ -1,36 +1,73 @@
 
 public class Overloaded {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        printMessage (10);
-        printMessage (9.185f);
-        printMessage (10, 9.185f);
-        printMessage (10, "hello", 100_000_000_000l);
-        printMessage ();
+        printMessage(calculate(10));
+        printMessage(calculate(99.1254));
+        printMessage(calculate(10, 10));
+        printMessage(calculate(9.9, 10.1));
+
+        calculate(1.9, 20.5, 5.01);
+
+        printMessage(10);
+        printMessage(9.185f);
+        printMessage(10, 9.185f);
+        printMessage(10, "hello", 100_000_000_000l);
+        printMessage();
 
     }
 
-    private static void printMessage (int number) {
+    private static int calculate(int x) {
+
+        return x * 1;
+
+    }
+
+    private static int calculate(int x, int y) {
+
+        return x * y;
+
+    }
+
+    private static double calculate(double x) {
+
+        return x * 1.0;
+
+    }
+
+    private static double calculate(double x, double y) {
+
+        return x * y;
+
+    }
+
+    private static void calculate(double x, double y, double z) {
+
+        printMessage(x, y, z);
+
+    }
+
+    private static void printMessage(int number) {
 
         System.out.println(number + " is an integer");
 
     }
 
-    private static void printMessage (float number) {
+    private static void printMessage(double number) {
 
-        System.out.println(number + " is a float");
+        System.out.println(number + " is a double");
 
     }
 
-    private static void printMessage (int firstNumber, float lastNumber) {
+    private static void printMessage(int firstNumber, double lastNumber) {
 
         System.out.println(firstNumber + " is an integer");
-        System.out.println(lastNumber + " is a float");
+        System.out.println(lastNumber + " is a double");
 
     }
 
-    private static void printMessage (int firstNumber, String middleString, long lastNumber) {
+    private static void printMessage(int firstNumber, String middleString, long lastNumber) {
 
         System.out.println(firstNumber + " is an integer");
         System.out.println(middleString + " is a string");
@@ -38,9 +75,20 @@ public class Overloaded {
 
     }
 
-    private static void printMessage () {
+    private static void printMessage(double firstNumber, double middleString, double lastNumber) {
 
-        System.out.println ("no method passed by");
+        System.out.println(firstNumber + " is an integer");
+        System.out.println(middleString + " is a string");
+        System.out.println(lastNumber + " is a long");
+
+        System.out.println(firstNumber * middleString * lastNumber);
 
     }
+
+    private static void printMessage() {
+
+        System.out.println("no method passed by");
+
+    }
+
 }
