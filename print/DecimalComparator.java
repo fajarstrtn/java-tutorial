c47a737285637a1a9d29444ee76d9191c86863ad
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-02 16:55:20 +0700

    style: set indentation for DecimalComparator.java and EqualSumChecker.java files

--- print/DecimalComparator.java
+++ print/DecimalComparator.java
@@ -1,17 +1,18 @@
+
 public class DecimalComparator {
 
-    public static void main (String[] args) {
-        
-        System.out.println (areEqualByThreeDecimalPlaces (10.123, 10.124));
+    public static void main(String[] args) {
+
+        System.out.println(areEqualByThreeDecimalPlaces(10.123, 10.124));
 
     }
-    
-    public static boolean areEqualByThreeDecimalPlaces (double firstNumber, double lastNumber) {
-        
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double lastNumber) {
+
         int num1 = (int) (firstNumber * 1000);
         int num2 = (int) (lastNumber * 1000);
 
         return num1 == num2;
-        
+
     }
 }
--- print/EqualSumChecker.java
+++ print/EqualSumChecker.java
@@ -1,14 +1,15 @@
+
 public class EqualSumChecker {
 
-    public static void main (String[] args) {
-        
-        System.out.println (hasEqualSum (1, 1, 2));
-        
+    public static void main(String[] args) {
+
+        System.out.println(hasEqualSum(1, 1, 2));
+
     }
-    
-    public static boolean hasEqualSum (int x, int y, int z) {
-        
+
+    public static boolean hasEqualSum(int x, int y, int z) {
+
         return x + y == z;
-        
+
     }
 }
