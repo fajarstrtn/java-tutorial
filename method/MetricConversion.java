8aec134b68d844c88e0bc0c4a871f14f34abdf65
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-08 02:03:38 +0700

    refactor: verify arguments for negative values

--- method/MetricConversion.java
+++ method/MetricConversion.java
@@ -13,18 +13,28 @@ public class MetricConversion {
 
     private static double convertToCentiMeters(int inches) {
 
+        if (inches < 0) {
+            System.err.println("negative inches value is not allowed");
+            return 0;
+        }
+
         return inches * 2.54;
 
     }
 
     private static double convertToCentiMeters(int feet, int inches) {
 
+        if (feet < 0 || inches < 0) {
+            System.err.println("Negative feet or inches value is not allowed");
+            return 0;
+        }
+
 /*      this code below is simpler but has low-readability
 
         return ((feet * 12) + inches) * 2.54;
         
         try change this with: */
-
+        
         int feetToInches = feet * 12;
         int totalInches = feetToInches + inches;
         double result = convertToCentiMeters(totalInches);
