f32849bc690d01af4ad22c4cfddffb1e30d22064
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-14 22:29:16 +0700

    refactor: add overloaded methods for validation purpose

--- method/Duration.java
+++ method/Duration.java
@@ -4,16 +4,54 @@ public class Duration {
     public static void main(String[] args) {
 
         getDurationString(3821);
+        getDurationString(0);
+        getDurationString(-100);
+        getDurationString(5, 1000);
+        getDurationString(0, 0);
+        getDurationString(-1, -20);
+
+    }
+
+    private static boolean isValid(int minutes, int seconds) {
+
+        return minutes >= 0 && seconds >= 0;
+
+    }
+
+    private static boolean isValid(int seconds) {
+
+        return seconds >= 0;
 
     }
 
     private static void getDurationString(int seconds) {
 
-        int toHours = seconds / 3600;
-        int toMinutes = (seconds % 3600) / 60;
-        int toSeconds = (seconds % 3600) % 60;
+        if (isValid(seconds)) {
 
-        System.out.println(toHours + "h " + toMinutes + "m " + toSeconds + "s");
+            int toHours = seconds / 3600;
+            int toMinutes = (seconds % 3600) / 60;
+            int toSeconds = (seconds % 3600) % 60;
 
+            System.out.println(toHours + "h " + toMinutes + "m " + toSeconds + "s");
+
+        } else {
+
+            System.err.println("Input is out of range");
+
+        }
+    }
+
+    private static void getDurationString(int minutes, int seconds) {
+
+        if (isValid(minutes, seconds)) {
+
+            int toSeconds = (minutes * 60) + seconds;
+            getDurationString(toSeconds);
+
+        } else {
+
+            System.err.println("Input is out of range");
+
+        }
     }
 }
