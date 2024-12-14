3f60652c4dd0b6ed295a0dc0393670a154c47874
fajarsatriatna <fajarsatriatna24@gmail.com>
2024-12-14 23:35:28 +0700

    feat: add PlayingCat.java file

---
+++ method/PlayingCat.java
@@ -0,0 +1,21 @@
+
+public class PlayingCat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Is cat playing? " + isCatPlaying(true, 26));
+        System.out.println("Is cat playing? " + isCatPlaying(true, 46));
+        System.out.println("Is cat playing? " + isCatPlaying(false, 27));
+        System.out.println("Is cat playing? " + isCatPlaying(false, 24));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        if (summer)
+            return temperature >= 25 && temperature <= 45; 
+        else
+            return temperature >= 25 && temperature <= 35;
+
+    }
+}
