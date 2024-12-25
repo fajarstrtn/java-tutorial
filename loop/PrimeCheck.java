8543ebdc2a13c76f39ebd082121e1f383e149e4a
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-25 23:32:54 +0700

    feat: add PrimeCheck.java file

---
+++ loop/PrimeCheck.java
@@ -0,0 +1,25 @@
+
+public class PrimeCheck {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        System.out.println(isPrime(number) ? "Prime" : "Not Prime");
+
+    }
+
+    static boolean isPrime(int n) {
+
+        if (n < 2)
+            return false;
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+        
+    }
+
+}
