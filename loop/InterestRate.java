21b386d2eabb7d63c1c50705627bf047cb18cd1c
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-25 20:21:40 +0700

    feat: add InterestRate.java file

---
+++ loop/InterestRate.java
@@ -0,0 +1,22 @@
+
+public class InterestRate {
+
+    public static void main(String[] args) {
+
+        double amount = 10_000;
+
+        for (double rate = 1.0; rate <= 5.0; rate++) {
+
+            double interestAmount = calculateInterest(amount, rate);
+            System.out.println(amount + " at " + rate + "% interest = " + interestAmount);
+
+        }
+        
+    }
+
+    private static double calculateInterest(double amount, double rate) {
+
+        return (amount * (rate / 100));
+
+    }
+}
