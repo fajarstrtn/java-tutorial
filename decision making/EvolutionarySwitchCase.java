01d30b7811256a9781f45858568ae894cd9b74d8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-26 19:19:27 +0700

    refactor: refactor EvolutionarySwitchCase.java file

--- decision making/EvolutionarySwitchCase.java
+++ decision making/EvolutionarySwitchCase.java
@@ -5,7 +5,7 @@ public class EvolutionarySwitchCase {
 
         Days selectedDay = Days.TUESDAY;
         String day = null;
-    
+
         switch (selectedDay) {
             case Days.MONDAY ->
                 day = "monday";
@@ -24,8 +24,10 @@ public class EvolutionarySwitchCase {
         }
 
         String dayStat = switch (selectedDay) {
-            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> "weekday";
-            case SATURDAY, SUNDAY -> "weekend";
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY ->
+                "weekday";
+            case SATURDAY, SUNDAY ->
+                "weekend";
         };
 
         System.out.println("today is " + day + " and this is " + dayStat);
@@ -33,20 +35,54 @@ public class EvolutionarySwitchCase {
         Integer number = 5;
 
         String printedValue = switch (number) {
-            case 1 -> "one";
-            case 2 -> "two";
-            case 3 -> "three";
-            case 4 -> "four";
-            case 5 -> "five";
-            case 6 -> "six";
-            case 7 -> "seven";
-            case 8 -> "eight";
-            case 9 -> "nine";
-            case 10 -> "ten";
-            default -> "out of range";
+            case 1 ->
+                "one";
+            case 2 ->
+                "two";
+            case 3 ->
+                "three";
+            case 4 ->
+                "four";
+            case 5 ->
+                "five";
+            case 6 ->
+                "six";
+            case 7 ->
+                "seven";
+            case 8 ->
+                "eight";
+            case 9 ->
+                "nine";
+            case 10 ->
+                "ten";
+            default ->
+                "out of range";
         };
 
         System.out.println(printedValue);
+
+        Object object = 11.5;
+
+        String typeCheckedString = switch (object) {
+            case Integer i ->
+                "integer: " + i;
+            case Long l ->
+                "long: " + l;
+            case Double d ->
+                "double: " + d;
+            case Boolean b ->
+                "boolean: " + b;
+            case String s when s.length() >= 10 ->
+                "long string: " + s;
+            case String s ->
+                "short string: " + s;
+            case null ->
+                "null object";
+            default ->
+                "uknown object";
+        };
+
+        System.out.println(typeCheckedString);
 
     }
 }
