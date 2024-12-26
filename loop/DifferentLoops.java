6279394bdf1b9938f2feceb72bd160fe85f444d9
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-26 18:40:57 +0700

    feat: add DifferentLoops.java file

---
+++ loop/DifferentLoops.java
@@ -0,0 +1,18 @@
+
+public class DifferentLoops {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; ++i) {
+            System.out.println(i);
+        }
+
+        int j = 1;
+
+        while (j <= 5) {
+            System.out.println(j);
+            ++j;
+        }
+
+    }
+}
