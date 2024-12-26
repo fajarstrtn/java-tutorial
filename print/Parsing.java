e43e1099aa6503fd4236f1931845bc44fc48e0dd
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-26 18:58:26 +0700

    feat: add Parsing.java file

---
+++ print/Parsing.java
@@ -0,0 +1,13 @@
+
+public class Parsing {
+
+    public static void main(String[] args) {
+
+        String currentYear = "2024";
+        int yearOfBirth = 1998;
+
+        System.out.println("current age: " + (Integer.parseInt(currentYear) - yearOfBirth));
+        System.out.println("concated string: " + currentYear + String.valueOf(yearOfBirth));
+
+    }
+}
