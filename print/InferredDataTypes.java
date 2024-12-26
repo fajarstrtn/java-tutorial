8d7ee3ff63712b9d0c7e97de0c8f7eda466db8d5
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-26 20:37:48 +0700

    feat: add InferredDataTypes.java file

---
+++ print/InferredDataTypes.java
@@ -0,0 +1,20 @@
+
+public class InferredDataTypes {
+
+    public static void main(String[] args) {
+
+        var name = "john doe";
+        var age = 21;
+        var address = "boulevard street no.101";
+
+/*      variable value should be initialized right away
+        because the data type is determined at compile-time.
+        
+        var hobby; will cause an error. */
+        
+        System.out.println("hi my name is " + name);
+        System.out.println("i am " + age + " years-old");
+        System.out.println("i lived in " + address);
+
+    }
+}
