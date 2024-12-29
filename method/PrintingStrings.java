68fb3c5445382d37a1f1812f6aa8e29946332940
Fajar Satriatna <fajarsatriatna24@gmail.com>
2024-12-29 22:28:54 +0700

    feat: add PrintingStrings.java file

---
+++ method/PrintingStrings.java
@@ -0,0 +1,17 @@
+
+public class PrintingStrings {
+
+    public static void main(String[] args) {
+
+        printName("john doe");
+        printName("sergey hart");
+        printName("lucy benington");
+
+    }
+
+    private static void printName(String name) {
+
+        System.out.println("hi my name is " + name);
+
+    }
+}
