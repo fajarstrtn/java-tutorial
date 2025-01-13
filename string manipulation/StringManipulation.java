12559d98a241b273cc88cbe86a545a72900dcfef
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-01-13 15:06:26 +0700

    refactor: add string methods like toLowerCase and toUpperCase

--- string manipulation/StringManipulation.java
+++ string manipulation/StringManipulation.java
@@ -9,55 +9,59 @@ public class StringManipulation {
 
         System.out.println(decodedByte);
 
-        String cityName = "Amsterdam";
+        String cityName = "amsterdam";
 
-        System.out.println("Length of cityName is " + cityName.length());
+        System.out.println("length of cityName is " + cityName.length());
 
-        System.out.println("Char at 3rd position is " + cityName.charAt(2));
+        System.out.println("char at 3rd position is " + cityName.charAt(2));
 
-        System.out.println("Substring from 2nd position is " + cityName.substring(1));
+        System.out.println("substring from 2nd position is " + cityName.substring(1));
 
-        System.out.println("Substring start at 3rd position, then get 4 chars is " + cityName.substring(2, 4));
+        System.out.println("substring start at 3rd position, then get 4 chars is " + cityName.substring(2, 4));
 
-        System.out.println("Concat with \'city\' becomes " + cityName.concat(" city"));
+        System.out.println("concat with \'city\' becomes " + cityName.concat(" city"));
 
-        System.out.println("What is exactly index position of A? " + cityName.indexOf('A'));
+        System.out.println("what is exactly index position of A? " + cityName.indexOf('A'));
 
-        System.out.println("What is exactly last index position of am? " + cityName.lastIndexOf("am"));
+        System.out.println("what is exactly last index position of am? " + cityName.lastIndexOf("am"));
 
-        System.out.println("Is Amsterdam equal with AMSTERDAM in case-sensitive way? " + cityName.equals("AMSTERDAM"));
+        System.out.println("is Amsterdam equal with AMSTERDAM in case-sensitive way? " + cityName.equals("AMSTERDAM"));
 
-        System.out.println("Is Amsterdam equal with AMSTERDAM in incase-sensitive way?  " + cityName.equalsIgnoreCase("AMSTERDAM"));
+        System.out.println("is Amsterdam equal with AMSTERDAM in incase-sensitive way?  " + cityName.equalsIgnoreCase("AMSTERDAM"));
 
-        System.out.println("Compare Amsterdam with amsterdam lexicographically, then it becomes "
+        System.out.println("compare Amsterdam with amsterdam lexicographically, then it becomes "
                 + cityName.compareTo("amsterdam"));
 
-        System.out.println("Compare with ignoring case between Amsterdam and amsterdam lexicographically, then it becomes "
+        System.out.println("compare with ignoring case between Amsterdam and amsterdam lexicographically, then it becomes "
                 + cityName.compareToIgnoreCase("amsterdam"));
 
-        System.out.println("Format Amsterdam to be in lowercase is " + cityName.toLowerCase());
+        System.out.println("format Amsterdam to be in lowercase is " + cityName.toLowerCase());
 
-        System.out.println("Format Amsterdam to be in upper case is " + cityName.toUpperCase());
+        System.out.println("format Amsterdam to be in upper case is " + cityName.toUpperCase());
 
-        System.out.println("Trim this new string \'hello     \' and it becomes " + "hello     ".trim());
+        System.out.println("trim this new string \'hello     \' and it becomes " + "hello     ".trim());
 
-        System.out.println("Does Amsterdam contain ster? " + cityName.contains("ster"));
+        System.out.println("does Amsterdam contain ster? " + cityName.contains("ster"));
 
-        System.out.println("Does Amsterdam start with \'Arm\'? " + cityName.startsWith("Arm"));
+        System.out.println("does Amsterdam start with \'Arm\'? " + cityName.startsWith("Arm"));
 
-        System.out.println("Does Amsterdam ends with \'dam\'? " + cityName.endsWith("dam"));
+        System.out.println("does Amsterdam ends with \'dam\'? " + cityName.endsWith("dam"));
+
+        System.out.println("uppercase of Amsterdam is " + cityName.toUpperCase());
+
+        System.out.println("lowercase of Amsterdam is " + cityName.toLowerCase());
 
         String emptyString = "";
 
         String whiteSpacesString = "       ";
 
-        System.out.println("Is the 1st string empty? " + emptyString.isEmpty());
+        System.out.println("is the 1st string empty? " + emptyString.isEmpty());
 
-        System.out.println("Is the 1st string blank? " + emptyString.isBlank());
+        System.out.println("is the 1st string blank? " + emptyString.isBlank());
 
-        System.out.println("Is the 2nd string empty? " + whiteSpacesString.isEmpty());
+        System.out.println("is the 2nd string empty? " + whiteSpacesString.isEmpty());
 
-        System.out.println("Is the 2nd string blank? " + whiteSpacesString.isBlank());
+        System.out.println("is the 2nd string blank? " + whiteSpacesString.isBlank());
 
     }
 }
