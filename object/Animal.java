1d10930fb1ab61965ba9b77a585d16f068a9b735
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-01-20 15:45:02 +0700

    feat: add Animal.java file

---
+++ object/Animal.java
@@ -0,0 +1,87 @@
+
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+
+public class Animal {
+
+    private static final Logger logger = System.getLogger(Animal.class.getName());
+
+    private String name;
+
+    private String species;
+
+    private Boolean isMammals;
+
+    public Animal() {
+
+    }
+
+    public Animal(String name, String species, Boolean isMammals) {
+
+        this.name = name;
+        this.species = species;
+        this.isMammals = isMammals;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public void setSpecies(String species) {
+
+        this.species = species;
+
+    }
+
+    public void setIsMammals(Boolean isMammals) {
+
+        this.isMammals = isMammals;
+
+    }
+
+    public String getName() {
+
+        return this.name;
+
+    }
+
+    public String getSpecies() {
+
+        return this.species;
+
+    }
+
+    public Boolean getIsMammals() {
+
+        return this.isMammals;
+
+    }
+
+    public static void main(String[] args) {
+
+        Animal cat = new Animal("mio", "felis_catus", true);
+
+        logger.log(Level.INFO, cat.getName() + "-" + cat.getSpecies() + "-" + cat.getIsMammals());
+
+        Animal dog = new Animal();
+
+        dog.setName("loki");
+        dog.setSpecies("family_canidae");
+        dog.setIsMammals(true);
+
+        logger.log(Level.INFO, dog.getName() + "-" + dog.getSpecies() + "-" + dog.getIsMammals());
+
+        Animal frog = new Animal();
+
+        frog.setName("leiya");
+        frog.setSpecies("anora");
+        frog.setIsMammals(false);
+
+        logger.log(Level.INFO, frog.getName() + "-" + frog.getSpecies() + "-" + frog.getIsMammals());
+
+    }
+
+}
