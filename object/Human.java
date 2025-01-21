d70073f2c2fc7ac2dfe7b90704e1c9e16457b963
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-01-22 00:35:56 +0700

    style: clean up Human.java file

--- object/Human.java
+++ object/Human.java
@@ -1,3 +1,4 @@
+
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 
@@ -20,6 +21,7 @@ public class Human {
 
     }
 
+    @Override
     public String toString() {
 
         return "hi, my name is " + this.name + " and i am " + this.age + " years-old";
@@ -33,12 +35,11 @@ public class Human {
         jack.name = "jack hummings";
         jack.age = 28;
 
-        logger.log(Level.INFO, jack.toString());
+        logger.log(Level.INFO, jack);
 
         Human gerard = new Human("gerard way", 25);
 
-        logger.log(Level.INFO, gerard.toString());
+        logger.log(Level.INFO, gerard);
 
     }
-
 }
