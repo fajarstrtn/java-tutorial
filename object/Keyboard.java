4ef7b5ac339ccf405d8d88dae5967c1c67ebb123
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-01-29 12:49:05 +0700

    feat: add Keyboard.java, MechanicalKeyboard.java, RegularKeyboard.java, and Peripheral.java files

---
+++ object/Keyboard.java
@@ -0,0 +1,118 @@
+
+public class Keyboard {
+
+    private String brand;
+
+    private String model;
+
+    public Keyboard() {
+    }
+
+    public Keyboard(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void displayInfo(String classType) {
+        System.out.println("this is " + classType + " class");
+        System.out.println("brand: " + this.brand);
+        System.out.println("model: " + this.model);
+    }
+
+    public void typeText(String text) {
+        System.out.println("your text on keyboard is \"" + text + "\"");
+    }
+
+}
+
+class MechanicalKeyboard extends Keyboard {
+
+    private String switchType;
+
+    public MechanicalKeyboard() {
+        super();
+    }
+
+    public MechanicalKeyboard(String switchType) {
+        this.switchType = switchType;
+    }
+
+    public MechanicalKeyboard(String switchType, String brand, String model) {
+        super(brand, model);
+        this.switchType = switchType;
+    }
+
+    public String getSwitchType() {
+        return switchType;
+    }
+
+    public void setSwitchType(String switchType) {
+        this.switchType = switchType;
+    }
+
+    @Override
+    public void displayInfo(String classType) {
+        super.displayInfo(classType);
+        System.out.println("switch type: " + this.switchType);
+    }
+
+    @Override
+    public void typeText(String text) {
+        System.out.println("your text on mechanical keyboard is \"" + text + "\"");
+    }
+
+}
+
+class RegularKeyboard extends Keyboard {
+
+    private Boolean hasNumericPad;
+
+    public RegularKeyboard() {
+        super();
+    }
+
+    public RegularKeyboard(Boolean hasNumericPad) {
+        this.hasNumericPad = hasNumericPad;
+    }
+
+    public RegularKeyboard(Boolean hasNumericPad, String brand, String model) {
+        super(brand, model);
+        this.hasNumericPad = hasNumericPad;
+    }
+
+    public Boolean getHasNumericPad() {
+        return hasNumericPad;
+    }
+
+    public void setHasNumericPad(Boolean hasNumericPad) {
+        this.hasNumericPad = hasNumericPad;
+    }
+
+    @Override
+    public void displayInfo(String classType) {
+        super.displayInfo(classType);
+        System.out.println("numeric pad: " + this.hasNumericPad);
+    }
+
+    @Override
+    public void typeText(String text) {
+        System.out.println("your text on regular keyboard is \"" + text + "\"");
+    }
+
+}
---
+++ object/Peripheral.java
@@ -0,0 +1,33 @@
+
+public class Peripheral {
+
+    public static void main(String[] args) {
+
+        var keyboard = new Keyboard();
+
+        keyboard.setBrand("logitech");
+        keyboard.setModel("wired");
+
+        keyboard.displayInfo(Keyboard.class.getName().toLowerCase());
+        keyboard.typeText("lorem ipsum dolor sit amet");
+
+        var mechanical = new MechanicalKeyboard();
+
+        mechanical.setBrand("razer");
+        mechanical.setModel("mechanical");
+        mechanical.setSwitchType("cherry mx red");
+
+        mechanical.displayInfo(MechanicalKeyboard.class.getName().toLowerCase());
+        mechanical.typeText("lorem ipsum dolor sit amet");
+
+        var regular = new RegularKeyboard();
+
+        regular.setBrand("dell");
+        regular.setModel("kb216");
+        regular.setHasNumericPad(true);
+
+        regular.displayInfo(RegularKeyboard.class.getName().toLowerCase());
+        regular.typeText("lorem ipsum dolor sit amet");
+
+    }
+}
