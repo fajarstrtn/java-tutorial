4b98a4d4f1bab986ab5a8e886c846d90931522ca
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-10 15:46:58 +0700

    feat: add Card.java and Table.java files

---
+++ pack/Table.java
@@ -0,0 +1,11 @@
+package pack;
+
+import pack.utils.Card;
+
+public class Table {
+
+    public static void main(String[] args) {
+        Card.shuffle();
+    }
+   
+}
---
+++ pack/utils/Card.java
@@ -0,0 +1,13 @@
+package pack.utils;
+
+public class Card {
+
+    private Card() {
+
+    }
+
+    public static void shuffle() {
+        System.out.println("card is being shuffled");
+    }
+
+}
