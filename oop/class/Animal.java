afd1d6aae735d14fd89696125d0d4a74f654828a
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-12 21:15:17 +0700

    style: reformat Animal.java code base; chore: move Animal.java to class folder

--- oop/class/Animal.java
+++ oop/class/Animal.java
@@ -7,105 +7,79 @@ public class Animal {
     private static final Logger logger = System.getLogger(Animal.class.getName());
 
     private String name;
-
     private String species;
-
     private Boolean isMammals;
 
     public Animal() {
-
     }
 
     public Animal(String name, String species, Boolean isMammals) {
-
         this.name = name;
         this.species = species;
         this.isMammals = isMammals;
-
     }
 
     public void setName(String name) {
-
         this.name = name;
-
-    }
-
-    public void setSpecies(String species) {
-
-        this.species = species;
-
-    }
-
-    public void setIsMammals(Boolean isMammals) {
-
-        this.isMammals = isMammals;
-
     }
 
     public String getName() {
-
         return this.name;
+    }
 
+    public void setSpecies(String species) {
+        this.species = species;
     }
 
     public String getSpecies() {
-
         return this.species;
-
     }
 
-    public Boolean getIsMammals() {
+    public void isMammals(Boolean isMammals) {
+        this.isMammals = isMammals;
+    }
 
+    public Boolean isMammals() {
         return this.isMammals;
-
     }
 
     @Override
     public String toString() {
-
         return String.format("""
         {
             "name" : "%s",
             "species" : "%s",
             "isMammals" : "%b"
-        }
-        """, this.name, this.species, this.isMammals);
-
+        }""", this.name, this.species, this.isMammals);
     }
 
     public static void main(String[] args) {
 
         Animal cat = new Animal("mio", "felis_catus", true);
-
         logger.log(Level.INFO, cat);
 
         Animal dog = new Animal();
-
         dog.setName("loki");
         dog.setSpecies("family_canidae");
-        dog.setIsMammals(true);
-
+        dog.isMammals(true);
         logger.log(Level.INFO, dog);
 
         Animal frog = new Animal();
-
         frog.setName("leiya");
         frog.setSpecies("anora");
-        frog.setIsMammals(false);
-
+        frog.isMammals(false);
         logger.log(Level.INFO, frog);
 
         Animal butterfly = new Animal();
-
         butterfly.setName("nora");
         butterfly.setSpecies("lepidoptera");
-        butterfly.setIsMammals(false);
-
+        butterfly.isMammals(false);
         logger.log(Level.INFO, String.format("""
-        "name": "%s",
-        "species": "%s",
-        "isMammals": "%b"
-        """, butterfly.getName(), butterfly.getSpecies(), butterfly.getIsMammals()));
+        {
+            "name" : "%s",
+            "species" : "%s",
+            "isMammals" : "%b"
+        }""", butterfly.getName(), butterfly.getSpecies(), butterfly.isMammals()));
 
     }
 
