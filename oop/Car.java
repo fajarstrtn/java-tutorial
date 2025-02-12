b1cf9291945bf6be874ec5517d9b5230dfec07c0
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-12 21:19:41 +0700

    style: reformat Car.java code base

--- oop/Car.java
+++ oop/Car.java
@@ -2,35 +2,29 @@
 public class Car {
 
     private String name;
-
-    private Double fuelTankCapacity;
-
+    private Double tankCapacity;
     private Double cc;
 
-    public Car(String name, Double fuelTankCapacity, Double cc) {
-
+    public Car(String name, Double tankCapacity, Double cc) {
 /*      variable shadowing in java occurs when a variable declared in a subclass or
         a method hides or "shadows" a variable of the same name declared in the superclass or
         an outer scope. this can create confusion if not handled carefully,
         as it may lead to unintended behavior. */
         name = name;
-        fuelTankCapacity = fuelTankCapacity;
+        tankCapacity = tankCapacity;
         cc = cc;
-
     }
 
     public void inform(String name) {
-
         System.out.println(name);
-
     }
 
     public static void main(String[] args) {
 
         var ferrari = new Car("ferrari spyder", 86D, 3855D);
 
-/*      this will print "justin" instead of "ferrari spyder". */
-        ferrari.inform("justin");
+/*      it returns "becky" instead of "ferrari spyder". */
+        ferrari.inform("becky");
 
     }
 
