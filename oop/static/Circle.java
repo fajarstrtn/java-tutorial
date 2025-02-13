27f95a26665f0a7eb003b5dd331d6160192f7952
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-13 13:22:50 +0700

    style: reformat Circle.java code base; chore: move Circle.java to static folder

--- oop/static/Circle.java
+++ oop/static/Circle.java
@@ -1,16 +1,15 @@
 
 public class Circle {
 
-    final double pi = 3.14;
+    final double PI = 3.14;
 
     public static void main(String[] args) {
 
         Circle circle = new Circle();
 
 /*      override a final field will cause an error
-        circle.pi = (double) 22 / 7; */
-
-        System.out.println("pi value is " + circle.pi);
+        circle.PI = (double) 22 / 7; */
+        System.out.println("pi value is " + circle.PI);
 
     }
 
