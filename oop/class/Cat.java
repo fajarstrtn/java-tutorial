54b4ca75e49a5b55040be2ea62058901c91105e5
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-13 15:14:20 +0700

    style: reformat Cat.java

--- oop/class/Cat.java
+++ oop/class/Cat.java
@@ -1,67 +1,23 @@
 
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
-
 public class Cat {
-
-    private static final Logger logger = System.getLogger(Cat.class.getName());
 
     String name;
     String breed;
-    int age;
     String color;
-    static final String ADOPTED_IN = "belgium";
-
-    public Cat() {
-    }
-
-    public Cat(String name, String breed, int age, String color) {
-        this.name = name;
-        this.breed = breed;
-        this.age = age;
-        this.color = color;
-    }
-
-    public boolean isPersian() {
-        return breed.equalsIgnoreCase("persian");
-    }
-
-    @Override
-    public String toString() {
-        return name + "-" + breed + "-" + age + "-" + color + "-" + ADOPTED_IN;
-    }
+    int age;
 
     public static void main(String[] args) {
 
         Cat mio = new Cat();
-
-/*      user cannot reassign value to the final attribute. for example:
-        ADOPTED_IN = "malaysia"; */
         mio.name = "mio";
         mio.breed = "persian";
-        mio.age = 12;
         mio.color = "white";
+        mio.age = 12;
 
-        logger.log(Level.INFO, mio);
-        logger.log(Level.INFO, "mio is persian cat, isn't it? " + mio.isPersian());
-
-        Cat jolly = new Cat("jolly", "siamese", 9, "white");
-
-        logger.log(Level.INFO, jolly);
-        logger.log(Level.INFO, "jolly is persian cat, isn't it? " + jolly.isPersian());
-
-        Cat jack = new Cat();
-
-/*      if attributes are not initialized, java will set the default values to themselves. */
-        logger.log(Level.INFO, jack);
-
-        Cat lily = new Cat();
-        lily.name = "lily";
-        lily.breed = "ragdoll";
-        lily.age = 7;
-        lily.color = "grey";
-
-        logger.log(Level.INFO, "my cat's name is " + lily.name + " and she is a " + lily.breed);
+        System.out.println("cat's name: " + mio.name);
+        System.out.println("cat's breed: " + mio.breed);
+        System.out.println("cat's color: " + mio.color);
+        System.out.println("cat's age: " + mio.age);
 
     }
 
