a52d035e47b727dcd040f065af5c63c52cbdcf97
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-13 13:21:08 +0700

    style: reformat Cat.java code base; chore: move Cat.java to class folder

--- oop/class/Cat.java
+++ oop/class/Cat.java
@@ -7,52 +7,40 @@ public class Cat {
     private static final Logger logger = System.getLogger(Cat.class.getName());
 
     String name;
-
     String breed;
-
     int age;
-
     String color;
+    static final String ADOPTED_IN = "belgium";
 
-/*  set attribute to final.
-    final String adoptedIn = "indonesia"; */
     public Cat() {
-
     }
 
     public Cat(String name, String breed, int age, String color) {
-
         this.name = name;
         this.breed = breed;
         this.age = age;
         this.color = color;
-
     }
 
     public boolean isPersian() {
-
-        return this.breed.equalsIgnoreCase("persian");
-
+        return breed.equalsIgnoreCase("persian");
     }
 
     @Override
     public String toString() {
-
-        return this.name + "-" + this.breed + "-" + this.age + "-" + this.color;
-
+        return name + "-" + breed + "-" + age + "-" + color + "-" + ADOPTED_IN;
     }
 
     public static void main(String[] args) {
 
         Cat mio = new Cat();
 
+/*      user cannot reassign value to the final attribute. for example:
+        ADOPTED_IN = "malaysia"; */
         mio.name = "mio";
         mio.breed = "persian";
         mio.age = 12;
         mio.color = "white";
-
-/*      user cannot reassign value to the final attribute.
-        mio.adoptedIn = "malaysia"; */
 
         logger.log(Level.INFO, mio);
         logger.log(Level.INFO, "mio is persian cat, isn't it? " + mio.isPersian());
@@ -68,7 +56,6 @@ public class Cat {
         logger.log(Level.INFO, jack);
 
         Cat lily = new Cat();
-
         lily.name = "lily";
         lily.breed = "ragdoll";
         lily.age = 7;
