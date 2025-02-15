05dab09ca06f177c9c3dc96f8c604e0016a8d25f
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-15 23:21:44 +0700

    style: reformat Car.java code base

--- oop/class/Car.java
+++ oop/class/Car.java
@@ -1,30 +1,59 @@
 
 public class Car {
 
-    private String name;
-    private Double tankCapacity;
-    private Double cc;
+    String name;
+    double tankCapacity;
+    double cc;
 
-    public Car(String name, Double tankCapacity, Double cc) {
-/*      variable shadowing in java occurs when a variable declared in a subclass or
-        a method hides or "shadows" a variable of the same name declared in the superclass or
-        an outer scope. this can create confusion if not handled carefully,
-        as it may lead to unintended behavior. */
+    /* This constructor declaration leads to an error which the variables are set to default values.
+        
+    public Car(String name, double tankCapacity, double cc) {
         name = name;
         tankCapacity = tankCapacity;
         cc = cc;
     }
+    
+    Use "this" keyword to refer to the class attributes instead of param variables. */
+    public Car(String name, double tankCapacity, double cc) {
+        this.name = name;
+        this.tankCapacity = tankCapacity;
+        this.cc = cc;
+    }
 
     public void inform(String name) {
+        /* Variable shadowing occurs when a subclass or method declares a variable 
+        with the same name as one in the superclass or outer scope, potentially causing confusion 
+        and unintended behavior. */
         System.out.println(name);
+    }
+
+    public void inform(String name, double tankCapacity, double cc) {
+        System.out.println(name);
+        System.out.println(tankCapacity);
+        System.out.println(cc);
+        System.out.println(this.name);
+        System.out.println(this.tankCapacity);
+        System.out.println(this.cc);
     }
 
     public static void main(String[] args) {
 
-        var ferrari = new Car("ferrari spyder", 86D, 3855D);
+        Car ferrari = new Car("ferrari spyder", 86, 3855);
 
-/*      it returns "becky" instead of "ferrari spyder". */
+        /* It returns "becky" instead of ferrari spyder. */
         ferrari.inform("becky");
+
+        /* Since class constructor has its declaration:
+
+        public Car(String name, double tankCapacity, double cc) {
+            name = name;
+            tankCapacity = tankCapacity;
+            cc = cc;
+        }
+
+        It only reinitialize param variables instead of class attributes.
+        To overcome this problem, set variables with "this" keyword. */
+        ferrari.inform("f8 tributo", 78, 3902);
 
     }
 
