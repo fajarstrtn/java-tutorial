7f4fa85045bdc0dc5c0b302d5d2cdda71090299a
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-15 23:36:30 +0700

    style: reformat Circle.java code base

--- oop/static/Circle.java
+++ oop/static/Circle.java
@@ -1,14 +1,13 @@
 
 public class Circle {
 
+    /* Constant field cannot be reassigned new value. */
     final double PI = 3.14;
 
     public static void main(String[] args) {
 
         Circle circle = new Circle();
 
-/*      override a final field will cause an error
-        circle.PI = (double) 22 / 7; */
         System.out.println("pi value is " + circle.PI);
 
     }
