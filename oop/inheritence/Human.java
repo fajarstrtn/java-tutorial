e0160499e64187f943ecad87f2c4080b8e197706
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-16 00:27:47 +0700

    style: reformat Human.java code base; chore: move Human.java to inheritence folder

--- oop/inheritence/Human.java
+++ oop/inheritence/Human.java
@@ -7,39 +7,33 @@ public class Human {
     private static final Logger logger = System.getLogger(Human.class.getName());
 
     public String name;
-
     public Integer age;
 
     public Human() {
-
     }
 
     public Human(String name, Integer age) {
-
         this.name = name;
         this.age = age;
-
     }
 
     @Override
     public String toString() {
-
-        return "hi, my name is " + this.name + " and i am " + this.age + " years-old";
-
+        return "hi, my name is " + name + " and i am " + age + " years-old";
     }
 
     public static void main(String[] args) {
 
         Human jack = new Human();
-
         jack.name = "jack hummings";
         jack.age = 28;
 
+        /* Java will automatically call toString method once its instance be printed. */
         logger.log(Level.INFO, jack);
 
         Human gerard = new Human("gerard way", 25);
-
         logger.log(Level.INFO, gerard);
 
     }
+
 }
