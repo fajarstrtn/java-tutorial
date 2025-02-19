560ff1a56dc0742e02767adba41ccf12d11554a0
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-20 01:04:16 +0700

    style: reformat Tree.java code base

--- oop/interface/Tree.java
+++ oop/interface/Tree.java
@@ -1,5 +1,8 @@
+
 public interface Tree {
 
+    /*  Interface cannot have instance variables (only static and final constants).
+    If a variable is assigned using '=', Java automatically shift this to constant variable. */
     String LEAF_COLOR = "green";
 
     final boolean HAS_BRANCH = true;
