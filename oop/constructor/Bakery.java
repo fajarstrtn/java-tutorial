66cb6e2d9f72de09fbe489bd3aafcc2dda4f2d5a
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-23 21:16:02 +0700

    feat: add BakeryShop.java and Bakery.java files

---
+++ oop/constructor/Bakery.java
@@ -0,0 +1,32 @@
+
+public class Bakery {
+
+    private String name;
+    private String type;
+    private boolean homeMade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isHomeMade() {
+        return homeMade;
+    }
+
+    public void setHomeMade(boolean homeMade) {
+        this.homeMade = homeMade;
+    }
+
+}
---
+++ oop/constructor/BakeryShop.java
@@ -0,0 +1,20 @@
+
+public class BakeryShop {
+
+    public static void main(String[] args) {
+
+        /* Java will provide default constructor once an object is created.
+        Then, it will be filled with default value. Unlike concrete method, constructor might at least be called once. */
+        Bakery bread = new Bakery();
+
+        bread.setName("bread");
+        bread.setType("wheat");
+        bread.setHomeMade(true);
+
+        System.out.println(bread);
+        System.out.println(bread.getName());
+        System.out.println(bread.getType());
+        System.out.println(bread.isHomeMade());
+
+    }
+}
