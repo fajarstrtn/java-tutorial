67b01e63f98c9f066903307ba272cc4bb371d819
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-02-24 07:34:40 +0700

    chore: change folder names; feat: add Currency.java file

---
+++ string/Currency.java
@@ -0,0 +1,23 @@
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Currency {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input your payment: ");
+        double payment = sc.nextDouble();
+        sc.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        System.out.println(us);
+
+        Locale localeIndia = new Locale.Builder().setLanguage("en").setRegion("IN").build();
+        String india = NumberFormat.getCurrencyInstance(localeIndia).format(payment);
+        System.out.println(india);
+
+    }
+}
