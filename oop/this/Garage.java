563e98c512312c9b643098f6b221db5cf0fda000
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-03-10 11:32:05 +0700

    feat: add Garage.java file

---
+++ oop/this/Garage.java
@@ -0,0 +1,69 @@
+
+public class Garage {
+
+    public static void main(String[] args) {
+
+        var car = new Vehicle();
+        car.setName("nissan gt-r");
+        car.setSeat(2);
+        car.setTankCapacity(74D);
+        car.setCc(3800D);
+
+        System.out.println(car.getName());
+        System.out.println(car.getSeat());
+        System.out.println(car.getTankCapacity());
+        System.out.println(car.getCc());
+
+    }
+}
+
+class Vehicle {
+
+    private String name;
+    private Integer seat;
+    private Double tankCapacity;
+    private Double cc;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name, Integer seat, Double tankCapacity, Double cc) {
+        this.name = name;
+        this.seat = seat;
+        this.tankCapacity = tankCapacity;
+        this.cc = cc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Integer seat) {
+        this.seat = seat;
+    }
+
+    public Double getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setTankCapacity(Double tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
+
+    public Double getCc() {
+        return cc;
+    }
+
+    public void setCc(Double cc) {
+        this.cc = cc;
+    }
+
+}
