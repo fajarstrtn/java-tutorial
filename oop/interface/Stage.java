761faf0d885e4b4f6fda737436e97b20673a76f8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-04 04:43:27 +0700

    refactor: implement multiple inheritance

--- oop/interface/Stage.java
+++ oop/interface/Stage.java
@@ -11,6 +11,15 @@ public class Stage {
         rallyCar.stopAutomatically();
         rallyCar.turnOffTheCar();
 
+        var superCar = new SuperCar();
+        superCar.turnOnTheCar();
+        superCar.moveForward();
+        superCar.stopManually();
+        superCar.moveBackward();
+        superCar.stopAutomatically();
+        superCar.drift();
+        superCar.turnOffTheCar();
+
     }
 
 }
@@ -36,6 +45,13 @@ interface StopActivity extends Power {
     void stopManually();
 
     void stopAutomatically();
+
+}
+
+/* interface is able to extend multiple interfaces. */
+interface Slalom extends MoveActivity, StopActivity {
+
+    void drift();
 
 }
 
@@ -69,6 +85,45 @@ class Car implements MoveActivity, StopActivity {
     @Override
     public void stopAutomatically() {
         System.out.println("The car stops automatically.");
+    }
+
+}
+
+class SuperCar implements Slalom {
+
+    @Override
+    public void drift() {
+        System.out.println("The super car is drifting.");
+    }
+
+    @Override
+    public void turnOnTheCar() {
+        System.out.println("Driver is turning on the super car.");
+    }
+
+    @Override
+    public void turnOffTheCar() {
+        System.out.println("Driver is turning off the super car.");
+    }
+
+    @Override
+    public void moveForward() {
+        System.out.println("The super car is moving forward.");
+    }
+
+    @Override
+    public void moveBackward() {
+        System.out.println("The super car is moving backward.");
+    }
+
+    @Override
+    public void stopManually() {
+        System.out.println("The super car stops manually.");
+    }
+
+    @Override
+    public void stopAutomatically() {
+        System.out.println("The super car stops automatically.");
     }
 
 }
