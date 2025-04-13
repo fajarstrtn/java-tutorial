eb5745393d1cc123682d38d71cff09eaae6ee931
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-13 19:38:12 +0700

    feat: invoke default method from class

---
+++ oop/interface/Universe.java
@@ -0,0 +1,44 @@
+
+public class Universe {
+
+    public static void main(String[] args) {
+
+        var planet = new Planet();
+        planet.revolve();
+
+    }
+
+}
+
+interface revolveOne {
+
+    default void revolve() {
+
+        System.out.println("revolveOne is called...");
+
+    }
+
+}
+
+interface revolveTwo {
+
+    default void revolve() {
+
+        System.out.println("revolveTwo is called...");
+
+    }
+
+}
+
+class Planet implements revolveOne, revolveTwo {
+
+    @Override
+    public void revolve() {
+
+        /* Since both revolveOne and revolveTwo provide a default method show(),
+        class Planet must override it explicitly. Using revolveOne.super.show();, it invokes show() from interface A. */
+        revolveOne.super.revolve();
+
+    }
+
+}
