c27e8a2c0166764b1869ca7e218a92a9051d40e0
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-13 16:43:28 +0700

    feat: create main method within interface

---
+++ oop/interface/Home.java
@@ -0,0 +1,13 @@
+
+/* In Java, an interface can have a main method starting from Java 8.
+However, it works a bit differently from how classes use it.
+It's not very common to do this in practice, but it's perfectly legal and can be useful for testing or demo purposes inside an interface. */
+public interface Home {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm home now.");
+
+    }
+
+}
