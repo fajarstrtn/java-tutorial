378273ffa50234803e267693fa435cd789edae9f
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-13 21:21:53 +0700

    refactor: add default method in functional interface

--- oop/interface/functional/Math.java
+++ oop/interface/functional/Math.java
@@ -3,9 +3,9 @@ public class Math {
 
     public static void main(String[] args) {
 
-        Addition calculation = (int x, int y) -> x + y;
+        Calculation calculation = (int x, int y) -> x + y;
         int result = calculation.add(10, 5);
-        System.out.println(result);
+        calculation.show(result);
 
     }
 
@@ -14,8 +14,13 @@ public class Math {
 /* Adding more than one abstract method will cause an error since functional interface only allows
 one abstract method. */
 @FunctionalInterface
-interface Addition {
+interface Calculation {
 
     int add(int x, int y);
+
+    /* Functional interface can have multiple default methods and static methods, but only one abstract method. */
+    default void show(int result) {
+        System.out.println("result: " + result);
+    }
 
 }
