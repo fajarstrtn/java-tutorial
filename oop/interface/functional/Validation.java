be7e2b4752704f9390492f22f488102b6e448546
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-26 00:37:08 +0700

    refactor: filter list of users based on its role

--- oop/interface/functional/Validation.java
+++ oop/interface/functional/Validation.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
+import java.util.Optional;
 
 public class Validation {
 
@@ -24,8 +25,8 @@ public class Validation {
         System.out.println(equals.test("Admin")); // true
 
         List<String> roles = List.of("Admin", "User", "Sys-Admin", "Guest", "User", "Admin", "Sys-Admin", "Guest");
-        Predicate<String> user = Predicate.isEqual("User");
-        roles.stream().filter(user).forEach(System.out::println); // User\nUser
+        Predicate<String> isUserEquals = Predicate.isEqual("User");
+        roles.stream().filter(isUserEquals).forEach(System.out::println); // User\nUser
 
         int number = -100;
         boolean validatedNumber = isLessThan0(number, x -> x < 0);
@@ -39,21 +40,108 @@ public class Validation {
         System.out.println(lessThanOrEquals20.test(20)); // true
 
         int[] intArray1 = {5, 0, -10, 7, -2, 8, 0, 4, -3, -7, 8};
-        IntPredicate positiveNumber = x -> x > 0;
-        IntStream.of(intArray1).sorted().filter(positiveNumber).forEach(System.out::println); // A list of array printed in new lines
+        IntPredicate isPositiveNumber = x -> x > 0;
+        IntStream.of(intArray1).sorted().filter(isPositiveNumber).forEach(System.out::println); // A list of array printed in new lines
+
+        List<User> users = List.of(
+                new User("Elara", null, "Quinn", (byte) 20D, "Admin"),
+                new User("Jaxon", null, "Rivera", (byte) 19D, "User"),
+                new User("Ezra", "Moon", "Walker", (byte) 25D, "User"),
+                new User("Luna", null, "Maris", (byte) 22D, "Admin"),
+                new User("Eleanor", "Rowan", "Pierce", (byte) 29D, "Sys-Admin")
+        );
+
+        List<User> filteredUsers = filterRole(users, user -> user.getRole().equals("User"));
+        filteredUsers.stream().map(user -> {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder fullName = sb.append(user.getFirstName());
+
+            if (Optional.ofNullable(user.getMiddleName()).isEmpty()) {
+                fullName.append(" ").append(user.getLastName());
+            } else {
+                fullName.append(" ").append(user.getMiddleName()).append(" ").append(user.getLastName());
+            }
+
+            return fullName.toString() + ": " + user.getRole();
+        }).forEach(System.out::println);
 
     }
 
-    public static boolean isLessThan0(int number, IntPredicate lessThan0) {
+    private static boolean isLessThan0(int number, IntPredicate lessThan0) {
 
         return lessThan0.test(number);
 
     }
 
-    public static boolean startsWith(String str, Predicate<String> startsWith) {
+    private static boolean startsWith(String str, Predicate<String> startsWith) {
 
         return startsWith.test(str);
 
+    }
+
+    private static List<User> filterRole(List<User> users, Predicate<User> filterRole) {
+        return users.stream().filter(filterRole).toList();
+    }
+
+}
+
+class User {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private byte age;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String firstName, String middleName, String lastName, byte age, String role) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
 }
