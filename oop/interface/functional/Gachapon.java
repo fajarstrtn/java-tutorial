c5caf64e674fa218e62fd8f70b596b19ed79d3d8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-26 18:57:07 +0700

    refactor: implement infinite stream with supplier interface

--- oop/interface/functional/Gachapon.java
+++ oop/interface/functional/Gachapon.java
@@ -2,6 +2,7 @@
 import java.util.function.Supplier;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class Gachapon {
 
@@ -14,6 +15,9 @@ public class Gachapon {
         // Supplier interface doesn't need any params, but returns a result.
         Supplier<String> item = () -> items.get(random.nextInt(items.size()));
         System.out.println(item.get());
+
+        // User can also create an infinite stream with Supplier interface.
+        Stream.generate(item).limit(5).forEach(System.out::println);
 
     }
 
