c9378afdd9a6584b98759c987187833ead41bed7
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-26 18:53:14 +0700

    feat: implement supplier interface

---
+++ oop/interface/functional/Gachapon.java
@@ -0,0 +1,20 @@
+
+import java.util.function.Supplier;
+import java.util.List;
+import java.util.Random;
+
+public class Gachapon {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        List<String> items = List.of("Evangelion", "Gundam", "One-Piece", "Naruto");
+
+        // Supplier interface doesn't need any params, but returns a result.
+        Supplier<String> item = () -> items.get(random.nextInt(items.size()));
+        System.out.println(item.get());
+
+    }
+
+}
