1649e70a590f1c35f389f11f1376dd7d80e5b82b
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-26 18:26:45 +0700

    refactor: implement function interface through map

--- oop/interface/functional/PriceTag.java
+++ oop/interface/functional/PriceTag.java
@@ -1,6 +1,7 @@
 
 import java.nio.channels.Pipe;
 import java.util.function.Function;
+import java.util.List;
 
 public class PriceTag {
 
@@ -38,6 +39,58 @@ public class PriceTag {
         Function<Integer, Integer> value = Function.identity();
         System.out.println(value.apply(100));
 
+        List<Employee> employees = List.of(
+                new Employee("James Dean", "Cashier", 5_000D),
+                new Employee("Jake Blake", "Sales", 6_000D),
+                new Employee("Livy Zheng", "Senior Sales", 6_500D),
+                new Employee("Lou Harbour", "Manager", 10_200D),
+                new Employee("Sidney Simpson", "Senior Manager", 25_000D)
+        );
+        Function<Employee, String> findEmployee = employee -> employee.getName();
+        employees.stream().map(findEmployee).forEach(System.out::println)
+        ;
+
+    }
+
+}
+
+class Employee {
+
+    private String name;
+    private String position;
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String position, Double salary) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
     }
 
 }
