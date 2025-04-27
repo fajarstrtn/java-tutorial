25f284bfd6b558a40052b35e2af045201ff1da2b
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-04-27 16:27:23 +0700

    refactor: implement nested interface within an interface

--- oop/interface/Garage.java
+++ oop/interface/Garage.java
@@ -9,6 +9,31 @@ public class Garage {
         Car.Break car2 = new SportCar();
         car2.stop();
 
+        Mode.Safe car3 = new SportCar();
+        car3.changeToNormal();
+
+    }
+
+}
+
+interface Mode {
+
+    interface Safe {
+
+        public void changeToNormal();
+
+        public void changeToEco();
+
+        public void changeToComfort();
+
+    }
+
+    interface Speed {
+
+        public void changeToSport();
+
+        public void changeToDrift();
+
     }
 
 }
@@ -29,7 +54,7 @@ class Car {
 
 }
 
-class SportCar implements Car.Move, Car.Break {
+class SportCar implements Car.Move, Car.Break, Mode.Safe {
 
     @Override
     public void drive() {
@@ -39,6 +64,21 @@ class SportCar implements Car.Move, Car.Break {
     @Override
     public void stop() {
         System.out.println("Driver stops the sport car.");
+    }
+
+    @Override
+    public void changeToNormal() {
+        System.out.println("Driver is changing his driving style to normal mode.");
+    }
+
+    @Override
+    public void changeToEco() {
+        System.out.println("Driver is changing his driving style to eco mode.");
+    }
+
+    @Override
+    public void changeToComfort() {
+        System.out.println("Driver is changing his driving style to comfort mode.");
     }
 
 }
