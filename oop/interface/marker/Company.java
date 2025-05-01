5ee8ee2a77a5f413950e0ed3175997c32ab014a7
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-05-01 15:04:31 +0700

    feat: implement marker interface through Company.java

---
+++ oop/interface/marker/Company.java
@@ -0,0 +1,32 @@
+
+public class Company {
+
+    public static void main(String[] args) {
+
+        Asset employee = new Employee();
+        Guest guest = new Guest();
+
+        if (employee instanceof Asset) {
+            System.out.println("Employee is an asset.");
+        } else {
+            System.out.println("Employee is not an asset.");
+        }
+
+        if (guest instanceof Asset) {
+            System.out.println("Guest is an asset.");
+        } else {
+            System.out.println("Guest is not an asset.");
+        }
+
+    }
+
+}
+
+interface Asset {
+}
+
+class Employee implements Asset {
+}
+
+class Guest {
+}
