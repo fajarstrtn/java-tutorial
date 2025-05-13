de84fe652b33b3c676def4262a1e3aa2d3e0d450
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-05-14 01:31:38 +0700

    chore: update code comments for clarity; feat: add equals method to compare two identical lists

--- collection/list/arraylist/Beverage.java
+++ collection/list/arraylist/Beverage.java
@@ -7,43 +7,59 @@ import java.util.ListIterator;
 public class Beverage {
 
     public static void main(String[] args) {
+        // The List Interface in Java extends the Collection Interface and is a part of the java.util package.
+        // It is used to store the ordered collections of elements.
+        // In a Java List, you can organize and manage the data sequentially.
+        // List is an interface, objects cannot be created of the type list.
+        // You always need a class that implements this List in order to create an object.
+        // List is an interface, implemented by the ArrayList class, pre-defined in java.util package. 
         List<String> menu = new ArrayList<>();
 
-        // Tropical Drift is at index 0.
+        // This method is used to add an element at the end of the list.
+        // Now, Tropical Drift is at index 0.
         menu.add("Tropical Drift");
 
-        // It allows duplicate value.
+        // List allows duplicate elements.
         menu.add("Tropical Drift");
 
         // It can add null values that depend on the implementation.
         menu.add(null);
 
         // If you don't specify the length of the array in the ArrayList constructor while creating the List object,
-        // using add(int index, Object) for any index i will throw an exception if we have not specified the values
+        // using add(int index, Object) for any index i will throw an exception if you have not specified the values
         // for 0 to i-1 index already.
         // 
         // If you try to add element at index 1 before adding elements at index 0
         // it will throw an error. It is always recommended to add elements in a particular index
         // only when the size is defined or to add them sequentially.
+        // 
+        // This method is used to add an element at a specific index in the list.
         menu.add(1, "Crimson Sunset");
 
-        System.out.println(menu);
+        System.out.println("First occurence of list: " + menu);
 
-        // menu.set(10, "Velvet Mocha Chill"); will throw java.lang.IndexOutOfBoundsException.
+        // This method takes an index and the updated element which needs to be inserted at that index.
+        // This menu.set(10, "Velvet Mocha Chill"); will throw java.lang.IndexOutOfBoundsException.
         menu.set(1, "Velvet Mocha Chill");
 
-        System.out.println(menu);
+        System.out.println("Second occurence of list: " + menu);
 
         // You can perform random access using index.
         System.out.println(menu.get(1));
 
+        // It returns the index of the first occurrence of the specified element in the list, or -1 if the element is not found.
         System.out.println("Index of \"Tropical Drift\" is " + menu.indexOf("Tropical Drift") + ".");
 
+        // This method is used to simply remove an object from the List.
+        // If there are multiple such objects, then the first occurrence of the object is removed.
         // You can remove elements. But removing elements while iterating it is not a recommended option.
         menu.remove(null);
+
+        // This method takes an integer value which simply removes the element present at that specific index in the list.
+        // After removing the element, all the elements are moved to the left to fill the space and the indices of the objects are updated.
         menu.remove(2);
 
-        System.out.println(menu);
+        System.out.println("Third occurence of list: " + menu);
 
         if (menu.contains("Velvet Mocha Chill")) {
             System.out.println("Yes, it is in the house now!");
@@ -58,16 +74,14 @@ public class Beverage {
         menu.add("Mocha");
         menu.add(3, "Affogato");
 
-        System.out.println(menu);
+        System.out.println("Fourth occurence of list: " + menu);
 
         // You can use indexOf() to find the first occurrence of an element in the list.
         int index = menu.indexOf("Mocha");
-
         System.out.println("Index of Mocha is " + index + ".");
 
         // Also, you can use lastIndexOf() to find the last occurrence of an element in the list.
         index = menu.lastIndexOf("Mocha");
-
         System.out.println("Last index of Mocha is " + index + ".");
 
         ListIterator<String> listIterator = menu.listIterator();
@@ -79,14 +93,14 @@ public class Beverage {
             }
         }
 
-        System.out.println(menu);
+        System.out.println("Fifth occurence of list: " + menu);
 
         // Adding 3 new beverages on the menu.
         menu.add("Frappuccino");
         menu.add("Iced Mocha");
         menu.add("Mocha");
 
-        System.out.println(menu);
+        System.out.println("Sixth occurence of list: " + menu);
 
         // This method is used to simply remove an object from the List.
         // If there are multiple such objects, then the first occurrence of the object is removed.
@@ -97,6 +111,7 @@ public class Beverage {
         // and the indices of the objects are updated.
         menu.remove(7);
 
+        // This method returns the element at the specified index in the list.
         // Accessing elements using get() method.
         String beverage = menu.get(1);
         System.out.println("I want to order " + beverage + ".");
@@ -145,6 +160,17 @@ public class Beverage {
         // It returns true if list is empty.
         boolean isEmpty = menu.isEmpty();
         System.out.println("Is menu empty? " + isEmpty + ".");
+
+        List<String> hisCoffee = new ArrayList<>(Arrays.asList("Honey Latte", "Pandan Lattle", "Piccolo"));
+        List<String> herCoffee = new ArrayList<>(Arrays.asList("Honey Latte", "Pandan Lattle", "Piccolo"));
+
+        // This equals method is used to compare two lists.
+        // It compares the lists as, both lists should have the same size, and
+        // all corresponding pairs of elements in the two lists are equal.
+        // This function has a single parameter which is object to be compared for equality.
+        // This method returns true if lists are equal.
+        boolean isEquals = hisCoffee.equals(herCoffee);
+        System.out.println("Are these two lists equal? " + isEquals + ".");
     }
 
 }
