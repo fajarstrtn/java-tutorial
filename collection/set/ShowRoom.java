74a76f896693ef54347f5f5354c893241ce27c47
Fajar Satriatna <fajarsatriatna24@gmail.com>
2025-05-18 18:45:47 +0700

    chore: update code comments for clarity

--- collection/set/ShowRoom.java
+++ collection/set/ShowRoom.java
@@ -10,8 +10,18 @@ public class ShowRoom {
         // When the iteration order is needed to be maintained this class is used.
         // When iterating through a HashSet the order is unpredictable, while a LinkedHashSet lets us iterate
         // through the elements in the order in which they were inserted.
+        // 
+        // LinkedHashSet in Java implements the Set interface of the Collection Framework.
+        // It combines the functionality of a HashSet with a LinkedList to maintain the insertion order of elements.
+        // Several functionality including:
+        // 1. Stores unique elements only.
+        // 2. Maintains insertion order.
+        // 3. Provides faster iteration compared to HashSet.
+        // 4. Allows null elements.
         Set<String> cars = new LinkedHashSet<>();
 
+        // The LinkedHashSet is an ordered version of HashSet that maintains a doubly-linked List across all elements.
+        // When the iteration order is needed to be maintained this class is used.
         cars.add("Audi");
         cars.add("VW");
         cars.add("Mercedes");
@@ -23,8 +33,9 @@ public class ShowRoom {
         cars.add("BYD");
         cars.add("Lotus");
         cars.add("Citroen");
+        cars.add(null);
 
-        System.out.println(cars); // [Audi, VW, Mercedes, BMW, Honda, Toyota, McLaren, Hyundai, BYD, Lotus, Citroen]
+        System.out.println(cars); // [Audi, VW, Mercedes, BMW, Honda, Toyota, McLaren, Hyundai, BYD, Lotus, Citroen, null]
     }
 
 }
