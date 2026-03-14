af5170b8c82259706999d08ca4ac2933ac9fd4fc
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 00:43:20 +0700

    feat: add Assignment.java file

---
+++ operator/Assignment.java
@@ -0,0 +1,32 @@
+
+public class Assignment {
+
+    public static void main(String[] args) {
+        String carName = "Lamborghini Aventador";
+
+        System.out.println("This is my " + carName); // Output: This is my Lamborghini Aventador
+
+        double price;
+        int horsePower;
+
+        price = 66_000.234;
+        horsePower = 45_000;
+
+        System.out.println("Mine has " + horsePower + " horse power"); // Output: Mine has 45000 horse power
+        System.out.println("It costs " + price + " USD");              // Output: It costs 66000.234 USD
+
+        int stock = 50;
+        System.out.println((stock += 10));     // Output: 60
+        System.out.println((stock -= 10));     // Output: 50
+        System.out.println((stock *= 10));     // Output: 500
+        System.out.println((stock /= 10));     // Output: 50
+        System.out.println((stock %= 10));     // Output: 0
+        System.out.println((stock &= 0b1010)); // Output: 0
+        System.out.println((stock |= 0b1010)); // Output: 10
+        System.out.println((stock ^= 0b0011)); // Output: 9
+        System.out.println((stock <<= 2));     // Output: 36
+        System.out.println((stock >>= 1));     // Output: 18
+        System.out.println((stock >>>= 1));    // Output: 9
+    }
+
+}
