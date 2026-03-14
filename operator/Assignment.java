e0fb3e2ebaf135bf485c4fbb19984e4bcde42ee4
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 01:22:31 +0700

    chore: update output message in Assignment.java file

--- operator/Assignment.java
+++ operator/Assignment.java
@@ -2,9 +2,9 @@
 public class Assignment {
 
     public static void main(String[] args) {
-        String carName = "Lamborghini Aventador";
+        String carName = "VW Golf GTI";
 
-        System.out.println("This is my " + carName); // Output: This is my Lamborghini Aventador
+        System.out.println("This is my " + carName); // Output: This is my VW Golf GTI
 
         double price;
         int horsePower;
@@ -16,17 +16,18 @@ public class Assignment {
         System.out.println("It costs " + price + " USD");              // Output: It costs 66000.234 USD
 
         int stock = 50;
-        System.out.println((stock += 10));     // Output: 60
-        System.out.println((stock -= 10));     // Output: 50
-        System.out.println((stock *= 10));     // Output: 500
-        System.out.println((stock /= 10));     // Output: 50
-        System.out.println((stock %= 10));     // Output: 0
-        System.out.println((stock &= 0b1010)); // Output: 0
-        System.out.println((stock |= 0b1010)); // Output: 10
-        System.out.println((stock ^= 0b0011)); // Output: 9
-        System.out.println((stock <<= 2));     // Output: 36
-        System.out.println((stock >>= 1));     // Output: 18
-        System.out.println((stock >>>= 1));    // Output: 9
+
+        System.out.println(stock += 10);     // Output: 60
+        System.out.println(stock -= 10);     // Output: 50
+        System.out.println(stock *= 10);     // Output: 500
+        System.out.println(stock /= 10);     // Output: 50
+        System.out.println(stock %= 10);     // Output: 0
+        System.out.println(stock &= 0b1010); // Output: 0
+        System.out.println(stock |= 0b1010); // Output: 10
+        System.out.println(stock ^= 0b0011); // Output: 9
+        System.out.println(stock <<= 2);     // Output: 36
+        System.out.println(stock >>= 1);     // Output: 18
+        System.out.println(stock >>>= 1);    // Output: 9
     }
 
 }
