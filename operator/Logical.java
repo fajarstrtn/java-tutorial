34c2d72472b87b93809f187ac7e08d887ada5d3d
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 01:00:01 +0700

    feat: add Logical.java file

---
+++ operator/Logical.java
@@ -0,0 +1,14 @@
+
+public class Logical {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(a && b); // Output: false
+        System.out.println(a || b); // Output: true
+        System.out.println(!a);     // Output: false
+        System.out.println(!b);     // Output: true
+    }
+
+}
