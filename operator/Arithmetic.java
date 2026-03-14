3ffed47c16428b0b0e6f93a590481cbb4f812fd5
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-14 16:43:34 +0700

    feat: add Arithmetic.java file

---
+++ operator/Arithmetic.java
@@ -0,0 +1,30 @@
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b); // Output: 15
+        System.out.println(a - b); // Output: 5
+        System.out.println(a * b); // Output: 50
+        System.out.println(a / b); // Output: 2
+        System.out.println(a % b); // Output: 0
+
+        int c = 7;
+        int d = 4;
+
+        System.out.println(c / d); // Output: 1
+        System.out.println(c % d); // Output: 3
+
+        double e = c / (double) d;
+
+        System.out.println("c / d = " + e); // Output: c / d = 1.75
+
+        String firstName = "John";
+        String lastName = "Doe";
+
+        System.out.println(firstName + lastName); // Output: JohnDoe
+    }
+
+}
