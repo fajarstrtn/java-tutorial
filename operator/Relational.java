dcd5454e1fa393e977a6c9bfafe08be79553fd72
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 00:53:42 +0700

    feat: add Relational.java file

---
+++ operator/Relational.java
@@ -0,0 +1,16 @@
+
+public class Relational {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+
+        System.out.println(a > b);  // Output: false
+        System.out.println(a < b);  // Output: true
+        System.out.println(a >= b); // Output: false
+        System.out.println(a <= b); // Output: true
+        System.out.println(a == b); // Output: false
+        System.out.println(a != b); // Output: true
+    }
+
+}
