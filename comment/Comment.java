2c6f26e2268cff7496e558fe0a00b06a2afbc25f
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-14 16:36:12 +0700

    feat: add Comment.java file

---
+++ comment/Comment.java
@@ -0,0 +1,16 @@
+
+public class Comment {
+
+    public static void main(String[] args) {
+        /*
+         * This is multi-line comment.
+         *
+         * These lines of code allow you to display output to the console. */
+        System.out.println("Hello World");
+
+        // This is a single-line comment.
+        System.out.println("Good Morning");
+        System.out.println("Good Night");// This is also a single-line comment.
+    }
+
+}
