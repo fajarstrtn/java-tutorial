d4d3d494d0d4ebd370f47a87c4e492333f1fb01b
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 01:24:10 +0700

    feat: add Ternary.java file

---
+++ operator/Ternary.java
@@ -0,0 +1,11 @@
+
+public class Ternary {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+
+        System.out.println((a > b) ? "a is greater than b" : "a is smaller than b"); // Output: a is greater than b
+    }
+
+}
