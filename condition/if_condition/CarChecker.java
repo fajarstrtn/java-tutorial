5d0843b631bf75f38f5b04b874f1be568e98f1bd
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 23:41:15 +0700

    feat: add CarChecker.java file

---
+++ condition/if_condition/CarChecker.java
@@ -0,0 +1,18 @@
+
+public class CarChecker {
+
+    public static void main(String[] args) {
+        String carName = "Renault";
+
+        if (carName.equals("Renault")) {
+            System.out.println("I\'m inside this car right now");
+        }
+
+        if (carName.equalsIgnoreCase("renault")) {
+            System.out.println("I\'m really inside this car right now");
+        }
+
+        System.out.println("Outside of if-block scope");
+    }
+
+}
