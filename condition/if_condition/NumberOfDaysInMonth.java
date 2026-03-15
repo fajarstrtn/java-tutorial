7ffbcf30c81d1d2ff5fe31857dc2fc3e5b52c41e
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 23:45:39 +0700

    feat: add NumberOfDaysInMonth.java file

---
+++ condition/if_condition/NumberOfDaysInMonth.java
@@ -0,0 +1,38 @@
+
+public class NumberOfDaysInMonth {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 ->
+                31;
+            case 2 ->
+                isLeapYear(year) ? 29 : 28;
+            case 4, 6, 9, 11 ->
+                30;
+            default ->
+                -1;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(2, 2020));  // Output: 29
+        System.out.println(getDaysInMonth(-8, 2021)); // Output: -1
+        System.out.println(getDaysInMonth(5, -2022)); // Output: -1
+        System.out.println(getDaysInMonth(10, 2023)); // Output: 31
+        System.out.println(getDaysInMonth(2, 2024));  // Output: 29
+        System.out.println(getDaysInMonth(2, 2025));  // Output: 28
+    }
+
+}
