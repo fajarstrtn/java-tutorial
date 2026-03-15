133c8f364b49f9aaf0fac7de8215cf897a83661e
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 23:43:03 +0700

    feat: add HumanChecker.java file

---
+++ condition/if_condition/HumanChecker.java
@@ -0,0 +1,17 @@
+
+public class HumanChecker {
+
+    public static void main(String[] args) {
+        boolean isHuman = true;
+        boolean isAlien = false;
+
+        if (isHuman) {
+            System.out.println("Human");
+        }
+
+        if (!isAlien) {
+            System.out.println("Alien");
+        }
+    }
+
+}
