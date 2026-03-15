caf89ec253c1ac1473cff357656bd0b9ef172f6d
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 14:41:43 +0700

    feat: add LocalVariable.java file

---
+++ variable/LocalVariable.java
@@ -0,0 +1,21 @@
+
+public class LocalVariable {
+
+    public int getNumber() {
+        int number = 99;
+        number += 100;
+        return number;
+    }
+
+    public static void main(String[] args) {
+        int localVariable = 100_000;
+
+        System.out.println(localVariable); // Output: 100000
+
+        // To access instance variables or methods in static context, you must create an object.
+        var instanceVariable = new LocalVariable();
+
+        System.out.println(instanceVariable.getNumber()); // Output: 199
+    }
+
+}
