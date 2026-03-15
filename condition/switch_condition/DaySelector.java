19a4e007b66add8dd9a6c871b883ec000339a6d9
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-16 00:05:53 +0700

    feat: add DaySelector.java file

---
+++ condition/switch_condition/DaySelector.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class DaySelector {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Insert the number: ");
+            int number = sc.nextInt();
+
+            String day;
+
+            day = switch (number) {
+                case 1 ->
+                    "Monday";
+                case 2 ->
+                    "Tuesday";
+                case 3 ->
+                    "Wednesday";
+                case 4 ->
+                    "Thursday";
+                case 5 ->
+                    "Friday";
+                case 6 ->
+                    "Saturday";
+                case 7 ->
+                    "Sunday";
+                default ->
+                    "Invalid value";
+            };
+
+            System.out.println(day);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        } finally {
+            System.out.println("Program is successfully terminated");
+        }
+    }
+
+}
