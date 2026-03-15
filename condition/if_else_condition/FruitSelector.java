9e5d77fba4f1c165b2fabc0174973e2daf7ba1aa
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-03-15 23:51:39 +0700

    feat: add FruitSelector.java file

---
+++ condition/if_else_condition/FruitSelector.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FruitSelector {
+
+    public static void main(String[] args) throws IOException {
+        int num = 0;
+        String fruit;
+
+        try (var br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("What is your favorite fruit: ");
+            fruit = br.readLine();
+
+            if (fruit.equals("Mango")) {
+                System.out.println("We give you " + fruit);
+                System.out.println("You got " + (++num) + " fruit");
+            } else {
+                System.out.println("Sorry, we do not have it");
+                System.out.println("You got " + (num) + " fruit");
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+    }
+
+}
