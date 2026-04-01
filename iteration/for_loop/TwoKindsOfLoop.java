c7dc6bf564fbb7a93e265eeaa51157e2f1ab89e8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:29:46 +0700

    feat: add TwoKindsOfLoop.java file

---
+++ iteration/for_loop/TwoKindsOfLoop.java
@@ -0,0 +1,16 @@
+
+public class TwoKindsOfLoop {
+
+    public static void main(String[] args) {
+        for (var i = 1; i <= 5; ++i) {
+            System.out.println(i);
+        }
+
+        var j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            ++j;
+        }
+    }
+
+}
