87d056aa4fbb5b87776df03a515d158eb70d6c26
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 12:01:47 +0700

    feat: add Termination.java file

---
+++ jump_condition/Termination.java
@@ -0,0 +1,21 @@
+
+public class Termination {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; ++i) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+        boolean isTerminated = true;
+
+        if (isTerminated) {
+            return; // The return statement will stop the program.
+        } else {
+            System.out.println("Hello!");
+        }
+    }
+
+}
