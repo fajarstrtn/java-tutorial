c075e980b507274ed35acd6f05f23ca97fd768ff
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 12:47:42 +0700

    feat: add Array.java file

---
+++ array/Array.java
@@ -0,0 +1,21 @@
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[5];
+        for (int intArrayValue : intArray) {
+            System.out.println(intArrayValue);
+        }
+
+        boolean[] booleanArray = new boolean[5];
+        for (boolean booleanArrayValue : booleanArray) {
+            System.out.println(booleanArrayValue);
+        }
+
+        String[] stringArray = new String[5];
+        for (String stringArrayValue : stringArray) {
+            System.out.println(stringArrayValue);
+        }
+    }
+
+}
