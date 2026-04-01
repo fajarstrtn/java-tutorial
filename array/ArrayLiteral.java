36d1f32132a48fbe6540310b3b894e8a8bc2adf3
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 12:46:05 +0700

    feat: add ArrayLiteral.java file

---
+++ array/ArrayLiteral.java
@@ -0,0 +1,12 @@
+
+public class ArrayLiteral {
+
+    public static void main(String[] args) {
+        int[] values = {15, 25, 35, 45, 55};
+        int size = values.length;
+        for (int i = 0; i < size; ++i) {
+            System.out.println(values[i]);
+        }
+    }
+
+}
