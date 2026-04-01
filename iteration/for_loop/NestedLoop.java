5fedb9c227058d3918538cad48f69a01d4c530f3
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:34:27 +0700

    feat: add NestedLoop.java file

---
+++ iteration/for_loop/NestedLoop.java
@@ -0,0 +1,13 @@
+
+public class NestedLoop {
+
+    public static void main(String[] args) {
+        for (var i = 1; i <= 5; ++i) {
+            for (var j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
