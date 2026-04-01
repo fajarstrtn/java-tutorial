010012386825dd2d38655884bd594188069cec98
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:14:30 +0700

    feat: add ArrayOfFruits.java file

---
+++ iteration/for_loop/ArrayOfFruits.java
@@ -0,0 +1,12 @@
+
+public class ArrayOfFruits {
+
+    public static void main(String[] args) {
+        String[] fruits = {"Mango", "Grape", "Apple", "Pineapple", "Guava"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+}
