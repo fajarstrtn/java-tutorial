27ed9ec232e04b4c3af871f79bd2ce9a5aa63374
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:35:00 +0700

    feat: add MaximumValue.java file

---
+++ iteration/for_loop/MaximumValue.java
@@ -0,0 +1,17 @@
+
+public class MaximumValue {
+
+    public static void main(String[] args) {
+        int[] array = {1, 10, 5, 4, 90, 19, 2, 0, 4, 5, 6, 99, 10, 11};
+        int maxValue = 0;
+
+        for (int value : array) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        System.out.println("Maximum value: " + maxValue);
+    }
+
+}
