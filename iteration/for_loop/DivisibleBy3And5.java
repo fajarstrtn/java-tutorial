b878e26c5ec5f7bf21cd06e8d229b85b467ea074
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:39:26 +0700

    feat: add DivisibleBy3And5.java file

---
+++ iteration/for_loop/DivisibleBy3And5.java
@@ -0,0 +1,34 @@
+
+public class DivisibleBy3And5 {
+
+    private static int sumAndPrintNumber(int sum, int i, int divNum) {
+        sum += i;
+        System.out.println(i + " is divisible by " + divNum);
+        return sum;
+    }
+
+    private static boolean isOutOfRange(int number) {
+        return number < 1 || number > 1000;
+    }
+
+    public static void main(String[] args) {
+        int number = 15;
+        int sum = 0;
+
+        if (isOutOfRange(number)) {
+            System.out.println("Invalid value");
+            return;
+        }
+
+        for (int i = 1; i <= number; ++i) {
+            if (i % 3 == 0) {
+                sum = sumAndPrintNumber(sum, i, 3);
+            } else if (i % 5 == 0) {
+                sum = sumAndPrintNumber(sum, i, 5);
+            }
+        }
+
+        System.out.println("Total sum: " + sum);
+    }
+
+}
