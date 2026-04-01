1a74684d2609ff1594f04197df7baf0ebc463416
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:31:26 +0700

    feat: add Pyramid.java file

---
+++ iteration/for_loop/Pyramid.java
@@ -0,0 +1,20 @@
+
+public class Pyramid {
+
+    public static void main(String[] args) {
+        var loop = 5;
+
+        for (var i = 1; i <= loop; ++i) {
+            for (var j = loop - i; j > 0; --j) {
+                System.out.print(" ");
+            }
+
+            for (var k = 1; k <= (i + (i - 1)); ++k) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+}
