7784858fff16f7222f6caf3f25072ab6ab5f41d3
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 12:49:31 +0700

    feat: add ArrayAlteration.java file

---
+++ array/ArrayAlteration.java
@@ -0,0 +1,25 @@
+
+public class ArrayAlteration {
+
+    private static int[] passByReference(int[] array) {
+        int size = array.length;
+        for (int i = 0; i < size; ++i) {
+            array[i] = (array[i] * 10) + 1;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        /*
+         * Use passByReference(int[] array) method to reveal that
+         * changes in that method will affect to its object. */
+        passByReference(array);
+
+        for (int newValue : array) {
+            System.out.println(newValue);
+        }
+    }
+
+}
