3543c625210e3334c364fc9601ea40fb8c8364c8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 19:20:29 +0700

    feat: add Exclude3Numbers.java file

---
+++ jump_condition/Exclude3Numbers.java
@@ -0,0 +1,19 @@
+
+public class Exclude3Numbers {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int loop = 20;
+
+        do {
+            if (i == 4 || i == 32 || i == 50) {
+                i += 2;
+                continue;
+            }
+
+            System.out.println(i);
+            i += 2;
+        } while (i <= loop);
+    }
+
+}
