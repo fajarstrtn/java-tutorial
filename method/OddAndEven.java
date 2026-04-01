48145dfef8f73ff2d12f8ed7a3ed8bd9ad1b6716
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 21:04:14 +0700

    feat: add OddAndEven.java file

---
+++ method/OddAndEven.java
@@ -0,0 +1,31 @@
+
+public class OddAndEven {
+
+    private static int validate(int number) {
+        if (number < 0 || number > 9999) {
+            System.out.println("Invalid value");
+            return -1;
+        }
+        return number;
+    }
+
+    private static void print(int validatedNumber) {
+        if (validatedNumber % 2 == 0) {
+            System.out.println(validatedNumber + " is even");
+        } else {
+            System.out.println(validatedNumber + " is odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 2015;
+        int validatedNumber = validate(number);
+
+        if (number != validatedNumber && validatedNumber == -1) {
+            return;
+        }
+
+        print(validatedNumber); // Output: 2015 is odd
+    }
+
+}
