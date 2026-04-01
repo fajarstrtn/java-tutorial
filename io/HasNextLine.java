3c0a10dba6ecf789cedc4efb0c467fe088d33730
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 19:50:32 +0700

    feat: add HasNextLine.java file

---
+++ io/HasNextLine.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class HasNextLine {
+
+    public static void main(String[] args) {
+        /*
+         * Use StringBuilder to create a mutable (modifiable) sequence of characters.
+         * \n newline character is used to separate input into lines.
+         * Without it, the Scanner would treat the entire input as a single line. */
+        String str = new StringBuilder()
+                .append("\tArsenal\n")
+                .append("     Aston Villa         \n")
+                .append("\t\t\tChelsea ")
+                .append("Manchester City       Manchester\n\n\tUnited")
+                .toString();
+
+        try (Scanner sc = new Scanner(str)) {
+            int i = 0;
+            while (sc.hasNextLine()) {
+                System.out.println(String.format("%d | %s", ++i, sc.nextLine()));
+            }
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        } finally {
+            System.out.println("Program is successfully terminated");
+        }
+    }
+
+}
