45f0eb356a620ad785c39c460a2e78a790585340
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:33:51 +0700

    feat: add NumberIteration.java file

---
+++ iteration/for_loop/NumberIteration.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class NumberIteration {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Insert the number: ");
+            int loop = sc.nextInt();
+            for (var i = 1; i <= loop; ++i) {
+                System.out.println(i);
+            }
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        } finally {
+            System.out.println("Program is successfully terminated");
+        }
+    }
+
+}
