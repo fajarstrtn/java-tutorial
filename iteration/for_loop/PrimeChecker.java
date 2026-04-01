8d38fb50068e5f2f7b0a5e784eb0ea310b9bb10d
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-01 11:32:33 +0700

    feat: add PrimeChecker.java file

---
+++ iteration/for_loop/PrimeChecker.java
@@ -0,0 +1,23 @@
+
+public class PrimeChecker {
+
+    static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        for (var i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 4;
+        System.out.println(isPrime(number) ? "Prime" : "Not Prime"); // Output: Not Prime
+    }
+
+}
