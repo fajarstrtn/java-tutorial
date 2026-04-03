a12a81da2fb6cb2d68422fa1297823a0aadb53cc
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-03 20:55:57 +0700

    feat: add GenericClass.java file

---
+++ oop/class/generic/GenericClass.java
@@ -0,0 +1,86 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * Type T in generic class is a placeholder for the type of variable.
+ * You can name it in different ways, but it is part of convention. */
+class Printer<T> {
+
+    private T value;
+
+    public Printer() {
+    }
+
+    public Printer(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void print() {
+        System.out.println(value);
+    }
+
+}
+
+class Lion {
+
+    private String name;
+    private int age;
+
+    public Lion() {
+    }
+
+    public Lion(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
+
+}
+
+public class GenericClass {
+
+    public static void main(String[] args) {
+        Printer<Integer> integerPrinter = new Printer<>();
+        integerPrinter.setValue(20);
+        integerPrinter.print();
+
+        Printer<Double> doublePrinter = new Printer<>();
+        doublePrinter.setValue(10.5);
+        doublePrinter.print();
+
+        var stringPrinter = new Printer<String>();
+        stringPrinter.setValue("Hello World");
+        stringPrinter.print();
+    }
+
+}
