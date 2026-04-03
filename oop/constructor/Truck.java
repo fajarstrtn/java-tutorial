5995e66f79b62939aabcedca39e673e9a4f4bbdd
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-03 21:08:37 +0700

    feat: add Truck.java file

---
+++ oop/constructor/Truck.java
@@ -0,0 +1,40 @@
+
+public class Truck {
+
+    private String name;
+    private double tankCapacity;
+    private double cc;
+
+    public Truck() {
+    }
+
+    /*
+     * initializer block contains the code that is always executed
+     * whenever an instance is created and
+     * runs each time when an object of the class is created. */
+    {
+        name = "Kamaz";
+        tankCapacity = 350;
+        cc = 11760;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public double getCc() {
+        return cc;
+    }
+
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        System.out.println(truck.getName());
+        System.out.println(truck.getTankCapacity());
+        System.out.println(truck.getCc());
+    }
+
+}
