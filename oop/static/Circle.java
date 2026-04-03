bde4044d514c3f856c00e090ca207183bbafbc6e
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-03 21:20:27 +0700

    feat: add Circle.java file

---
+++ oop/static/Circle.java
@@ -0,0 +1,11 @@
+
+public class Circle {
+
+    // Constant field cannot be reassigned a new value.
+    public static final double PI = 3.14;
+
+    public static void main(String[] args) {
+        System.out.println(Circle.PI);
+    }
+
+}
