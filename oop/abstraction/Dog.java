6f5acc0523e7b89ec7115659453f429c78ca5d7d
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-04-05 22:43:42 +0700

    feat: add Dog.java file

---
+++ oop/abstraction/Dog.java
@@ -0,0 +1,38 @@
+
+abstract class Animal {
+
+    private String name;
+
+    protected Animal() {
+        name = null;
+    }
+
+    protected Animal(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void makeSound();
+
+}
+
+public class Dog extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof... Woof... Woof...");
+    }
+
+    public static void main(String... args) {
+        Animal dog = new Dog();
+        dog.makeSound();
+    }
+
+}
